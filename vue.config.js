8516cb16e8dc46bc8a4e36199f05eecc359990de
nagasakimark <m.pook022@g.nagasaki-city.ed.jp>
2025-10-29 20:21:07 +0900

    Configure GitHub Pages deployment: update baseUrl for /trickortreat/ and add .nojekyll

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  baseUrl: './',
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/trickortreat/'
+    : '/',
   devServer: {
     disableHostCheck: true // Reference: https://github.com/vuejs-templates/webpack/issues/1205
   }
